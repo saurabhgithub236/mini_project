5415be0310d395a6704c9d943b11d007b4ea5553
rishikesh835 <rishikeshmishra835@gmail.com>
2024-01-11 10:09:57 +0530

    node and express installed

---
+++ app.js
@@ -0,0 +1,40 @@
+const express=require("express")
+const path=require("path")
+const fs=require("fs")
+const app=express()
+const hostname = '127.0.0.1'
+const port = 8000;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/contact', (req, res)=>{
+    
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+
+
+
+
+// START THE SERVER
+// app.listen(port, ()=>{
+//     console.log(`The application started successfully on port ${port}`);
+// });
+
+//starting the server
+app.listen(port, () => {
+    console.log(`The application has started on port http://${hostname}:${port}/`);
+})
